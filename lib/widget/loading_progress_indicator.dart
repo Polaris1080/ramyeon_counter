688bf3de8ce196bc954f193cc392146935343914
Polaris1080 <Polaris1080@outlook.jp>
2026-08-03 11:58:37 +0900

    ...

--- lib/widget/loading_progress_indicator.dart
+++ lib/widget/loading_progress_indicator.dart
@@ -8,6 +8,7 @@ class LoadingProgressIndicator extends StatelessWidget {
     this.duration,
   });
 
+  /* Argument */
   /// [LinearProgressIndicator] color
   @protected
   final Color? overrideColor;
