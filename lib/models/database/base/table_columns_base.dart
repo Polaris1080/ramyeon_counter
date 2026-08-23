2b1a9d6b387b6eb3795ce48621dbd8fdad9a38ee
Polaris1080 <Polaris1080@outlook.jp>
2026-08-23 16:45:40 +0900

    RamyeonDatabaseTables comment

--- lib/models/database/base/table_columns_base.dart
+++ lib/models/database/base/table_columns_base.dart
@@ -1,1 +1,10 @@
-abstract class TableColumnsBase {}
+// Package
+import 'package:meta/meta.dart';
+
+// Base
+import 'column_definition.dart';
+
+abstract class TableColumnsBase {
+  @mustBeOverridden
+  static List<ColumnConstraint> get tableDefinition => [];
+}
--- lib/models/database/ramyeon/ramyeon_database_tables.dart
+++ lib/models/database/ramyeon/ramyeon_database_tables.dart
@@ -1,10 +1,25 @@
 // Base
 import '../base/database_tables_base.dart';
+// Table
+import './table/barcode_table_columns.dart';
+import './table/company_table_columns.dart';
+import './table/ramyeon_table_columns.dart';
+import './table/rating_table_columns.dart';
+import './table/stock_table_columns.dart';
 
 enum RamyeonDatabaseTables implements DatabaseTablesBase {
+  /// [BarcodeTableColumns]
   barcode,
+
+  /// [CompanyTableColumns]
   company,
+
+  /// [RamyeonTableColumns]
   ramyeon,
+
+  /// [RatingTableColumns]
   rating,
+
+  /// [StockTableColumns]
   stock,
 }
