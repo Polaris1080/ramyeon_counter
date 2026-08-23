91d880f970e21aeffa3c763f53fb393eb06e8790
Polaris1080 <Polaris1080@outlook.jp>
2026-08-23 14:15:21 +0900

    new DatabaseTablesBase

--- lib/models/database/ramyeon/ramyeon_database_tables.dart
+++ lib/models/database/ramyeon/ramyeon_database_tables.dart
@@ -1,1 +1,9 @@
-enum RamyeonDatabaseTables { barcode, company, ramyeon, rating, stock }
+import 'package:ramyeon_counter/models/database/database_tables_base.dart';
+
+enum RamyeonDatabaseTables implements DatabaseTablesBase {
+  barcode,
+  company,
+  ramyeon,
+  rating,
+  stock,
+}
