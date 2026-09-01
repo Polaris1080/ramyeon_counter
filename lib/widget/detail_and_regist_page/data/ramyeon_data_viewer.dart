0bf165c06e4c179374b58de42f72a8a83ba20ea6
Polaris1080 <Polaris1080@outlook.jp>
2026-09-01 17:13:26 +0900

    RamyeonDataViewer 仮実装

--- lib/widget/detail_and_regist_page/data/ramyeon_data_viewer.dart
+++ lib/widget/detail_and_regist_page/data/ramyeon_data_viewer.dart
@@ -1,23 +1,57 @@
 // Package
+import 'package:darq/darq.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:ramyeon_counter/models/model/ramyeon/repository/ramyeon_repository.dart';
+import 'package:ramyeon_counter/models/model/ramyeon/repository/rating_repository.dart';
+import 'package:ramyeon_counter/models/model/ramyeon/repository/stock_repository.dart';
 // Widget
 import 'package:ramyeon_counter/widget/rating/rating_viewer.dart';
 
-class RamyeonDataViewer extends StatelessWidget {
-  const RamyeonDataViewer({super.key, required this.ramyeonId});
-
-  final int ramyeonId;
-
+class const RamyeonDataViewer({super.key, required final int ramyeonId})
+    extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dt = DateFormat('yyyy年MM月dd日');
     return Column(
       mainAxisSize: .min,
       spacing: 8,
       children: [
-        Text("ramyeon.brand"),
-        RatingViewer(vm: .new(1)),
-        Text('価格133円'),
-        Text('期間2025/4/31-2026/4/31'),
+        // TODO:仮実装
+        FutureBuilder(
+          future: RamyeonRepository().read(ramyeonId),
+          builder: (context, asyncSnapshot) {
+            return switch (asyncSnapshot.connectionState) {
+              .done => Text(asyncSnapshot.requireData.brand),
+              _ => Text("ramyeon.brand"),
+            };
+          },
+        ),
+        FutureBuilder(
+          future: RatingRepository().readByBrandId(ramyeonId),
+          builder: (context, asyncSnapshot) {
+            return switch (asyncSnapshot.connectionState) {
+              .done => RatingViewer(
+                vm: .new(asyncSnapshot.requireData.average((x) => x.rating)),
+              ),
+              _ => SizedBox(),
+            };
+          },
+        ),
+        FutureBuilder(
+          future: StockRepository().readByBrandId(ramyeonId),
+          builder: (context, asyncSnapshot) {
+            return switch (asyncSnapshot.connectionState) {
+              .done => Text(
+                '価格${asyncSnapshot.requireData.where((x) => x.purchaseDate.isAfter(DateTime.now().subtract(Duration(days: 365)))).average((x) => x.price)}円',
+              ),
+              _ => SizedBox(),
+            };
+          },
+        ),
+        Text(
+          '${dt.format(DateTime.now().subtract(Duration(days: 365)))}-${dt.format(DateTime.now())}',
+        ),
       ],
     );
   }
